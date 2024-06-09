596575a62b6ea933c3cab684214d028b5171e958
Greg Kinne <gkinne@gmail.com>
2024-06-09 16:25:09 -0400

    forgot to set non-dbo default schema for apps

--- hows-the-weather/PostDeploy/user_creation.sql
+++ hows-the-weather/PostDeploy/user_creation.sql
@@ -3,7 +3,7 @@ BEGIN
     IF NOT EXISTS (SELECT 1 FROM sys.database_principals WHERE name = 'hows-the-weather@kinne.solutions')
     BEGIN
         /* tsqllint-disable invalid-syntax */
-        CREATE USER [hows-the-weather@kinne.solutions] FOR EXTERNAL PROVIDER;
+        CREATE USER [hows-the-weather@kinne.solutions] FOR EXTERNAL PROVIDER WITH DEFAULT_SCHEMA=data;
         /* tsqllint-enable invalid-syntax */
     END
     IF NOT EXISTS (SELECT 1 FROM sys.database_principals WHERE name = 'db-deploy@kinne.solutions')
@@ -20,7 +20,7 @@ BEGIN
     IF NOT EXISTS (SELECT 1 FROM sys.database_principals WHERE name = 'hows-the-weather@kinne.dev')
     BEGIN
         /* tsqllint-disable invalid-syntax */
-        CREATE USER [hows-the-weather@kinne.dev] FOR EXTERNAL PROVIDER; 
+        CREATE USER [hows-the-weather@kinne.dev] FOR EXTERNAL PROVIDER WITH DEFAULT_SCHEMA=data; 
         /* tsqllint-enable invalid-syntax */
     END
     IF NOT EXISTS (SELECT 1 FROM sys.database_principals WHERE name = 'db-deploy@kinne.dev')
